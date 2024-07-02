0e39713e0d73ec05b413a5a8f5397f7e963a032c
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-02 15:52:57 -0400

    Change logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,15 +5,9 @@ const NavBar = () => {
     <>
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            <img
-              src="/Images/logo192.png"
-              alt="Logo"
-              width="30"
-              height="24"
-              className="d-inline-block align-text-top"
-            />
-            EventFlow
+          <a className="navbar-brand d-flex align-items-center" href="/">
+            <img src="/Images/logo1x.png" alt="Logo" width="50" className="d-inline-block align-text-top" />
+            <span className="mx-3">EventFlow</span>
           </a>
         </div>
       </nav>
