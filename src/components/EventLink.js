72dbf7946fc531f70a0487a548211f9273999a1b
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-02 11:10:13 -0400

    Create component for event links

---
+++ src/components/EventLink.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { useNavigate } from "react-router-dom"
+
+const EventLink = ({ eventInfo: { id, eventName, date } }) => {
+  const navigate = useNavigate()
+  const navigateTo = () => {
+    navigate(`/project/${id}`)
+  }
+
+  return (
+    <li id={id} onClick={navigateTo}>
+      <a href="#">
+        {eventName} on {date}
+      </a>
+    </li>
+  )
+}
+
+export default EventLink
