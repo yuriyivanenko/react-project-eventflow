37f17273ceb67df186f49c8a3f1469026d69c14f
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-02 11:07:11 -0400

    Add Home button to error page

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
 
-const Home = () => {
+const Error404 = () => {
   const navigate = useNavigate()
   const handleClick = () => {
     navigate("/")
@@ -28,4 +28,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Error404
