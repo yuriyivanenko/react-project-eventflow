fa70935e800e145460ca9b9d477acaeb78f2dd29
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-02 08:58:44 -0400

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@ import React from "react"
 import { createBrowserRouter } from "react-router-dom"
 import Home from "./pages/Home"
 import NewEvent from "./pages/NewProject"
+import Error404 from "./pages/Error404"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    // element: <App />,
     element: <Home />,
+    errorElement: <Error404 />,
   },
   {
     path: "/new_event",
