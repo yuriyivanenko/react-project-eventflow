4bd4b14b7c3ed54e5308ccf457151aad4dcfad49
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-03 15:31:41 -0400

    Deleting unecessary files for production. Updated favicon image. Updated README

--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ import "@eonasdan/tempus-dominus/dist/css/tempus-dominus.min.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min"
 import "bootstrap-icons/font/bootstrap-icons.css"
-import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<RouterProvider router={router} />)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
